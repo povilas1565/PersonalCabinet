b1005940c758185ac5696437adb7726439b54d20
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-27 12:40:29 +0300

    Репозитории для сущностей

---
+++ src/main/java/com/example/socialNetwork/repository/ImageRepository.java
@@ -0,0 +1,13 @@
+package com.example.socialNetwork.repository;
+
+import com.example.socialNetwork.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    Optional<Image> findByUserId(Long userId);
+}
