96054b0a2eba59509b3af90b64b62b3b2347b94b
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-27 08:54:51 +0300

    Базовые шаги работы над сервисом личного кабинета пользователя

---
+++ src/test/java/com/example/socialNetwork/InstaCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
