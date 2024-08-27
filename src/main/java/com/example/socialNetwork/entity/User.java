333b2484a357d5b3c935b6aeda63ca5e94ef43af
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-27 12:40:04 +0300

    Cущности из базы данных

---
+++ src/main/java/com/example/socialNetwork/entity/User.java
@@ -0,0 +1,96 @@
+package com.example.socialNetwork.entity;
+
+import com.example.socialNetwork.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, updatable = false)
+    private String username; // ник, логин
+
+    @Column(length = 3000)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(columnDefinition = "text")
+    private String info;
+
+    @Column(updatable = false)
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    private LocalDateTime createDate;
+
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
+    private Set<ERole> roles = new HashSet<>();
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createDate = LocalDateTime.now();
+    }
+
+    // Security
+
+    public User(Long id, String username, String password, String email,Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
