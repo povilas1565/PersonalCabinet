54d64dfc03abd543bd1d3b7a88e486522068ae59
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-30 10:20:21 +0300

    Репозиторий для Post

---
+++ src/main/java/com/example/socialNetwork/repository/PostRepository.java
@@ -0,0 +1,19 @@
+package com.example.socialNetwork.repository;
+
+import com.example.socialNetwork.entity.Post;
+import com.example.socialNetwork.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByOrderByCreateDate();
+
+    List<Post> findAllByUserOrderByCreateDateDesc(User user);
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+}
